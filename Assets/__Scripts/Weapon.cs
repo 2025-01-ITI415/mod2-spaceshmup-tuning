910e43eb89e23e367ee937b91d817f5cdaf9d7f1
IvanGudz0808 <vanyahudz@gmail.com>
2025-02-26 19:08:59 -0500

    made changes in file weapon.cs

--- Assets/__Scripts/Weapon.cs
+++ Assets/__Scripts/Weapon.cs
@@ -1,41 +1,32 @@
-﻿using System.Collections;
+﻿﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-/// <summary>
-/// This is an enum of the various possible weapon types.
-/// It also includes a "shield" type to allow a shield power-up.
-/// Items marked [NI] below are Not Implemented in the IGDPD book.
-/// </summary>
 public enum WeaponType
 {
-    none, // The default / no weapons
-    blaster, // A simple blaster
-    spread, // Two shots simultaneously
-    phaser, // [NI] Shots that move in waves
-    missile, // [NI] Homing missiles
-    laser, // [NI] Damage over time
-    shield // Raise shieldLevel
+    none,
+    blaster,
+    spread,
+    phaser,
+    missile,
+    laser,
+    shield
 }
 
-/// <summary>
-/// The WeaponDefinition class allows you to set the properties
-/// of a specific weapon in the Inspector. The Main class has
-/// an array of WeaponDefinitions that makes this possible.
-/// </summary>
 [System.Serializable]
 public class WeaponDefinition
 {
     public WeaponType type = WeaponType.none;
-    public string letter; // Letter to show on the power-up
-    public Color color = Color.white; // Color of Collar & power-up
-    public GameObject projectilePrefab; // Prefab for projectiles
+    public string letter;
+    public Color color = Color.white;
+    public GameObject projectilePrefab;
     public Color projectileColor = Color.white;
-    public float damageOnHit = 0; // Amount of damage caused
-    public float continuousDamage = 0; // Damage per second (Laser)
+    public float damageOnHit = 0;
+    public float continuousDamage = 0;
     public float delayBetweenShots = 0;
-    public float velocity = 20; // Speed of projectiles
+    public float velocity = 20;
 }
+
 public class Weapon : MonoBehaviour {
     static public Transform PROJECTILE_ANCHOR;
 
@@ -44,25 +35,21 @@ public class Weapon : MonoBehaviour {
     private WeaponType _type = WeaponType.none;
     public WeaponDefinition def;
     public GameObject collar;
-    public float lastShotTime; // Time last shot was fired
+    public float lastShotTime;
     private Renderer collarRend;
 
     private void Start()
     {
         collar = transform.Find("Collar").gameObject;
         collarRend = collar.GetComponent<Renderer>();
-
-        // Call SetType() for the default _type of WeaponType.none
         SetType(_type);
 
-        // Dynamically create an anchor for all Projectiles
         if(PROJECTILE_ANCHOR == null)
         {
             GameObject go = new GameObject("_ProjectileAnchor");
             PROJECTILE_ANCHOR = go.transform;
         }
 
-        // Find the fireDelegate of the root GameObject
         GameObject rootGO = transform.root.gameObject;
         if(rootGO.GetComponent<Hero>() != null)
         {
@@ -72,14 +59,8 @@ public class Weapon : MonoBehaviour {
 
     public WeaponType type
     {
-        get
-        {
-            return (_type);
-        }
-        set
-        {
-            SetType(value);
-        }
+        get { return (_type); }
+        set { SetType(value); }
     }
 
     public void SetType(WeaponType wt)
@@ -96,40 +77,49 @@ public class Weapon : MonoBehaviour {
         }
         def = Main.GetWeaponDefinition(_type);
         collarRend.material.color = def.color;
-        lastShotTime = 0; // You can fire immediately after _type is set.
+        lastShotTime = 0;
     }
 
     public void Fire()
     {
-        Debug.Log("Weapon Fired:" + gameObject.name);
-        // If this.gameObject is inactive, return
         if (!gameObject.activeInHierarchy) return;
-        // If it hasn't been enough time between shots, return
-        if (Time.time - lastShotTime < def.delayBetweenShots)
-        {
-            return;
-        }
+        if (Time.time - lastShotTime < def.delayBetweenShots) return;
+
         Projectile p;
         Vector3 vel = Vector3.up * def.velocity;
-        if (transform.up.y < 0)
-        {
-            vel.y = -vel.y;
-        }
+        if (transform.up.y < 0) vel.y = -vel.y;
+
         switch (type)
         {
             case WeaponType.blaster:
+                def.delayBetweenShots = 0.15f;
+                def.damageOnHit = 4f;
                 p = MakeProjectile();
                 p.rigid.velocity = vel;
                 break;
 
             case WeaponType.spread:
-                p = MakeProjectile(); // Make middle Projectile
+                def.delayBetweenShots = 0.7f;
+                def.damageOnHit = 8f;
+                p = MakeProjectile();
                 p.rigid.velocity = vel;
-                p = MakeProjectile(); // Make right Projectile
-                p.transform.rotation = Quaternion.AngleAxis(10, Vector3.back);
+                p = MakeProjectile();
+                p.transform.rotation = Quaternion.AngleAxis(15, Vector3.back);
                 p.rigid.velocity = p.transform.rotation * vel;
-                p = MakeProjectile(); // Make left Projectile
-                p.transform.rotation = Quaternion.AngleAxis(-10, Vector3.back);
+                p = MakeProjectile();
+                p.transform.rotation = Quaternion.AngleAxis(-15, Vector3.back);
+                p.rigid.velocity = p.transform.rotation * vel;
+                p = MakeProjectile();
+                p.transform.rotation = Quaternion.AngleAxis(30, Vector3.back);
+                p.rigid.velocity = p.transform.rotation * vel;
+                p = MakeProjectile();
+                p.transform.rotation = Quaternion.AngleAxis(-30, Vector3.back);
+                p.rigid.velocity = p.transform.rotation * vel;
+                p = MakeProjectile();
+                p.transform.rotation = Quaternion.AngleAxis(45, Vector3.back);
+                p.rigid.velocity = p.transform.rotation * vel;
+                p = MakeProjectile();
+                p.transform.rotation = Quaternion.AngleAxis(-45, Vector3.back);
                 p.rigid.velocity = p.transform.rotation * vel;
                 break;
         }
