04082764f9b0a8320bec765c160f3d59bf0f0a87
kkc76-RU <kkc76@scarletmail.rutgers.edu>
2025-02-26 23:22:23 -0500

    Change enemy health and speed
    I did a minor change to enemy health to 15 from 10 while increase the speed to 15 from 10.  I did this because the enemy are pretty tank with just blaster and the speed was already pretty fast.

--- Assets/__Scripts/Enemy.cs
+++ Assets/__Scripts/Enemy.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 public class Enemy : MonoBehaviour {
 
     [Header("Set in Inspector: Enemy")]
-    public float speed = 10f; // The speed in m/s
+    public float speed = 15f; // The speed in m/s
     public float fireRate = 0.3f; // Seconds/shot (Unused)
-    public float health = 10;
+    public float health = 15;
     public int score = 100; // Points earned for destroying this
     public float showDamageDuration = 0.1f; // # seconds to show damage
     public float powerUpDropChance = 1f; // Chance to drop a power-up
