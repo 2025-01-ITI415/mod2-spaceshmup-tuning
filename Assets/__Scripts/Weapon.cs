d85b5bbcdac6d5935ad6450f83a771888fac9a6e
kkc76-RU <kkc76@scarletmail.rutgers.edu>
2025-02-26 23:14:23 -0500

    Changes to weapon blaster and spread
    For blaster I increased the velocity of the projectile by 5 times to it. with this change it seems to increase the fire rate. while for the spread, I added extra projectiles while delaying the speed of the velocity by half. for this it seems to slow down the rate of fire, but I could be wrong.

--- Assets/__Scripts/Weapon.cs
+++ Assets/__Scripts/Weapon.cs
@@ -119,18 +119,24 @@ public class Weapon : MonoBehaviour {
         {
             case WeaponType.blaster:
                 p = MakeProjectile();
-                p.rigid.velocity = vel;
+                p.rigid.velocity = (5*vel);
                 break;
 
             case WeaponType.spread:
                 p = MakeProjectile(); // Make middle Projectile
-                p.rigid.velocity = vel;
+                p.rigid.velocity = (.5f * vel);
                 p = MakeProjectile(); // Make right Projectile
                 p.transform.rotation = Quaternion.AngleAxis(10, Vector3.back);
-                p.rigid.velocity = p.transform.rotation * vel;
+                p.rigid.velocity = p.transform.rotation * (.5f * vel);
+                p = MakeProjectile(); // Make middle right Projectile
+                p.transform.rotation = Quaternion.AngleAxis(5, Vector3.back);
+                p.rigid.velocity = p.transform.rotation * (.5f * vel);
                 p = MakeProjectile(); // Make left Projectile
                 p.transform.rotation = Quaternion.AngleAxis(-10, Vector3.back);
-                p.rigid.velocity = p.transform.rotation * vel;
+                p.rigid.velocity = p.transform.rotation * (.5f * vel);
+                p = MakeProjectile(); // Make middle left Projectile
+                p.transform.rotation = Quaternion.AngleAxis(-5, Vector3.back);
+                p.rigid.velocity = p.transform.rotation * (.5f * vel);
                 break;
         }
     }
