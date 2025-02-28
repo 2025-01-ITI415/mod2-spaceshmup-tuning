10406e734c0fa0efeae8433fa69e9a5e2dc4b8b5
Ethan Payne <pqynereturn@gmail.com>
2025-02-27 21:22:52 -0500

    changed weapon stats + enemy spawn rate
    spread now shoots 5 projectiles, increased damage and velocity to blaster and spread,
    increased enemy spawn rate

--- Assets/__Scripts/Weapon.cs
+++ Assets/__Scripts/Weapon.cs
@@ -131,6 +131,12 @@ public class Weapon : MonoBehaviour {
                 p = MakeProjectile(); // Make left Projectile
                 p.transform.rotation = Quaternion.AngleAxis(-10, Vector3.back);
                 p.rigid.velocity = p.transform.rotation * vel;
+                 p = MakeProjectile(); // Make 4th Projectile
+                p.transform.rotation = Quaternion.AngleAxis(-5, Vector3.back);
+                p.rigid.velocity = p.transform.rotation * vel;
+                p = MakeProjectile(); // Make 4th Projectile
+                p.transform.rotation = Quaternion.AngleAxis(5, Vector3.back);
+                p.rigid.velocity = p.transform.rotation * vel;
                 break;
         }
     }
